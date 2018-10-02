1e3f83f8c8c92793218a54e1e8d84fcc5b5840cc
zom <jonathonwhite5@gmail.com>
2018-10-02 15:38:44 -0400

    reduce time steps to 64

--- test_model_tf.py
+++ test_model_tf.py
@@ -28,20 +28,20 @@ with tf.Session() as sess:
 
     # PLOT PREDICTIONS
     mses = []
-    batch_x_train, batch_y_train = get_data.evaluate_on_train_data()
+    batch_x_train, batch_y_train = get_data.evaluate_on_train_data(samples=10)
     predictions = sess.run(y_pred, feed_dict={x: batch_x_train, y_true: batch_y_train})
     for ax, index in zip([ax1, ax2, ax3, ax4, ax5, ax6], [0, 1, 2, 3, 4, 5]):
         mse = sess.run(loss,
                        feed_dict={x: batch_x_train[index].reshape(1, -1), y_true: batch_y_train[index].reshape(1, -1)})
         mses.append(mse)
         ax[0].cla()
-        ax[0].plot(t, predictions[index, :128], color="blue")
-        ax[0].plot(t, predictions[index, 128:], color="red")
+        ax[0].plot(t, predictions[index, :64], color="blue")
+        ax[0].plot(t, predictions[index, 64:], color="red")
         ax[0].set_title("prediction [train set]")
         ax[0].text(0.5, 0.5, "MSE: " + str(mse), transform=ax[0].transAxes, backgroundcolor='white')
         ax[1].cla()
-        ax[1].plot(t, batch_y_train[index, :128], color="blue")
-        ax[1].plot(t, batch_y_train[index, 128:], color="red")
+        ax[1].plot(t, batch_y_train[index, :64], color="blue")
+        ax[1].plot(t, batch_y_train[index, 64:], color="red")
         ax[1].set_title("actual [train set]")
     plt.show()
     print("mses: ", mses)
