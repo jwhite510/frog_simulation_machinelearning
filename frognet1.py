10f913ed8a20d11382f1a22868314056ddf8a765
zom <jonathonwhite5@gmail.com>
2018-10-02 21:01:01 -0400

    more complex pulse shapes

--- frognet1.py
+++ frognet1.py
@@ -307,7 +307,7 @@ def generate_phi_w(N, nodes, amplitude):
 
 if __name__ == '__main__':
 
-    phi_w = generate_phi_w(N=2**6, nodes=15, amplitude=3)
+    phi_w = generate_phi_w(N=2**6, nodes=20, amplitude=4)
 
     # output 64 time step E
     E, t, w, dt, w0 = generateE_phi_vector(plot=True, phi_w=phi_w)
